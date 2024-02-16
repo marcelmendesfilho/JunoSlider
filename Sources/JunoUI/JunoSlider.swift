5c61c8999f3476ae81b5b5a05d69ff1ef4d8f7d2
Marcel Mendes Filho <marcel.mendes@gmail.com>
2024-02-16 15:49:08 -0300

    feat: adapting slider to meet VRP requirements

--- Sources/JunoUI/JunoSlider.swift
+++ Sources/JunoUI/JunoSlider.swift
@@ -2,12 +2,13 @@ import SwiftUI
 
 /// A slider that expands on selection.
 public struct JunoSlider: View {
-    @Binding var sliderValue: CGFloat
+    var sliderValue: CGFloat
     let maxSliderValue: CGFloat
     let baseHeight: CGFloat
     let expandedHeight: CGFloat
     let label: String
     let editingChanged: ((Bool) -> Void)?
+    let newSliderValue: ((CGFloat) -> Void)?
     
     @State private var isGestureActive: Bool = false
     @State private var startingSliderValue: CGFloat?
@@ -22,21 +23,18 @@ public struct JunoSlider: View {
     ///   - expandedHeight: The slider's height when selected (thus expanded)
     ///   - label: A string to describe what the data the slider represents
     ///   - editingChanged: An optional block that is called when the slider updates to sliding and when it stops
-    public init(sliderValue: Binding<CGFloat>, maxSliderValue: CGFloat, baseHeight: CGFloat = 9.0, expandedHeight: CGFloat = 20.0, label: String, editingChanged: ((Bool) -> Void)? = nil) {
-        self._sliderValue = sliderValue
+    public init(sliderValue: CGFloat, maxSliderValue: CGFloat, baseHeight: CGFloat = 9.0, expandedHeight: CGFloat = 20.0, label: String, editingChanged: ((Bool) -> Void)? = nil, newSliderValue: ((CGFloat) -> Void)? = nil) {
+        self.sliderValue = sliderValue
         self.maxSliderValue = maxSliderValue
         self.baseHeight = baseHeight
         self.expandedHeight = expandedHeight
         self.label = label
         self.editingChanged = editingChanged
+        self.newSliderValue = newSliderValue
     }
     
     public var body: some View {
         ZStack {
-            // visionOS (on device) does not like when drag targets are smaller than 40pt tall, so add an almost-transparent (as it still needs to be interactive) that enforces an effective minimum height. If the slider is tall than this on its own it's essentially just ignored.
-            Color.orange.opacity(0.0001)
-                .frame(height: 40.0)
-            
             Capsule()
                 .background {
                     GeometryReader { proxy in
@@ -78,7 +76,6 @@ public struct JunoSlider: View {
                 let percentagePointsIncreased = value.translation.width / sliderWidth
                 let initialPercentage = (startingSliderValue ?? sliderValue) / maxSliderValue
                 let newPercentage = min(1.0, max(0.0, initialPercentage + percentagePointsIncreased))
-                sliderValue = newPercentage * maxSliderValue
                 
                 if newPercentage == 0.0 && !isAtTrackExtremity {
                     isAtTrackExtremity = true
@@ -87,15 +84,13 @@ public struct JunoSlider: View {
                 } else if newPercentage > 0.0 && newPercentage < 1.0 {
                     isAtTrackExtremity = false
                 }
+                newSliderValue?(newPercentage * maxSliderValue)
             }
             .onEnded { value in
                 // Check if they just tapped somewhere on the bar rather than actually dragging, in which case update the progress to the position they tapped
                 if value.translation.width == 0.0 {
                     let newPercentage = value.location.x / sliderWidth
-                    
-                    withAnimation {
-                        sliderValue = newPercentage * maxSliderValue
-                    }
+                    newSliderValue?(newPercentage * maxSliderValue)
                 }
                 
                 startingSliderValue = nil
@@ -105,13 +100,6 @@ public struct JunoSlider: View {
         )
         .hoverEffect(.highlight)
         .animation(.default, value: isGestureActive)
-        .accessibilityRepresentation {
-            Slider(value: $sliderValue, in: 0.0 ... maxSliderValue, label: {
-                Text(label)
-            }, onEditingChanged: { editingChanged in
-                self.editingChanged?(editingChanged)
-            })
-        }
     }
     
     private var innerCirclePadding: CGFloat { expandedHeight * 0.15 }
